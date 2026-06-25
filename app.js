e80ec0f2e481c9efe3d627a4e9901a97c92e103f
luffy809 <ns7745922@gmail.com>
2026-06-25 11:58:56 +0530

    add

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: "https://reactmain-bru4.vercel.app"            //install cors
+    origin: "https://reactmain-4ou1.vercel.app"            //install cors
   })
 );
 app.use("/api", router)
